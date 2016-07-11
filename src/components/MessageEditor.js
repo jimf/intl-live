96626a9876eed85a40421e73dfb5549cfbcc0dd4
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2016-07-11 16:10:15 -0400

    MessageEditor: Add message/setMessage props
    Value and onChange handler for the editor.

--- src/components/MessageEditor.js
+++ src/components/MessageEditor.js
@@ -2,18 +2,28 @@ import { createFactory, PropTypes } from 'react';
 import Quill from 'react-quill';
 
 const quill = createFactory(Quill);
+const formats = [];
 
-const MessageEditor = ({ message }) => (
+/**
+ * Call props change handler with the text value of the editor on change.
+ */
+const handleChange = onChange => (value, delta, source, editor) =>
+    onChange(editor.getText());
+
+const MessageEditor = ({ message, setMessage }) => (
     quill({
         key: 'quill',
         theme: 'snow',
         toolbar: false,
-        value: message
+        formats,
+        value: message,
+        onChange: handleChange(setMessage)
     })
 );
 
 MessageEditor.propTypes = {
-    message: PropTypes.string.isRequired
+    message: PropTypes.string.isRequired,
+    setMessage: PropTypes.func.isRequired
 };
 
 export default MessageEditor;
